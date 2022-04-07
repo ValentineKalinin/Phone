324e6622cdb7a3f31012d2c9e6d165be35816c5c
”Valentine <kalvalkaltan@gmail.com>
2022-04-08 01:22:15 +0300

    1. Add new field model, getter and setter for it 2. Create new generic method

--- src/main/MobilePhone.java
+++ src/main/MobilePhone.java
@@ -4,45 +4,57 @@ import interfaces.IShortMessageService;
 import interfaces.IWorkable;
 import static main.Main.LOGGER;
 
+import java.util.List;
 import java.util.Objects;
 
 public class MobilePhone extends Phone implements IShortMessageService, IWorkable {
-    private String unlock;
+    private String unlockMethod;
+    private String phoneModel;
 
     public MobilePhone() {
     }
 
     public MobilePhone(Battery battery, Screen screen, String model) {
         super(battery, screen, model);
+        this.phoneModel = model;
     }
 
-    public String getUnlock() {
-        return unlock;
+    public String getUnlockMethod() {
+        return unlockMethod;
     }
 
-    public void setUnlock(String unlock) {
-        this.unlock = unlock;
+    public void setUnlockMethod(String unlockMethod) {
+        this.unlockMethod = unlockMethod;
+    }
+
+    public String getPhoneModel() {
+        return phoneModel;
+    }
+
+    public void setPhoneModel(String model) {
+        this.phoneModel = model;
     }
 
     @Override
     public String toString() {
         return "MobilePhone{" +
-                "unlock='" + unlock + '\'' +
+                "unlockMethod='" + unlockMethod + '\'' +
+                ", model='" + phoneModel + '\'' +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof MobilePhone)) return false;
         if (!super.equals(o)) return false;
         MobilePhone that = (MobilePhone) o;
-        return Objects.equals(unlock, that.unlock);
+        return unlockMethod.equals(that.unlockMethod) && phoneModel.equals(that.phoneModel);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), unlock);
+        return Objects.hash(super.hashCode(), unlockMethod, phoneModel);
     }
 
     @Override
@@ -58,5 +70,11 @@ public class MobilePhone extends Phone implements IShortMessageService, IWorkabl
     @Override
     public void workable() {
         LOGGER.info(getModel() + " is work!");
+    }
+
+    public static <T> void showOrder(List<T> list) {
+        for (T element : list) {
+            LOGGER.info(element);
+        }
     }
 }
