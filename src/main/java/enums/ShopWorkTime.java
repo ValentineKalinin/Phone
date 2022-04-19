891b9660a8e3e9619027376bb281fa099d835a45
”Valentine <kalvalkaltan@gmail.com>
2022-04-20 02:24:14 +0300

    Enum with methods

---
+++ src/main/java/enums/ShopWorkTime.java
@@ -0,0 +1,25 @@
+package main.java.enums;
+
+import static main.java.phone.Main.LOGGER;
+
+public enum ShopWorkTime {
+    FULL_TIME_WORK {
+        @Override
+        public void info() {
+            LOGGER.info("You can visit anytime store at " + Store.MAIN_OFFICE.getAddress());
+        }
+    },
+    EVENING_WORKING {
+        @Override
+        public void info() {
+            LOGGER.info("You can visit in the evening store at " + Store.MAIN_OFFICE.getAddress() + " or at " + Store.STORE_CENTRE.getAddress());
+        }
+    },
+    MORNING_WORKING {
+        @Override
+        public void info() {
+            LOGGER.info("You can visit in the morning store at " + Store.MAIN_OFFICE.getAddress() + " or at " + Store.STORE_WEST.getAddress());
+        }
+    };
+    public abstract void info();
+}
